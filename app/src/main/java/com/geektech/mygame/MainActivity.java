a65af141db3a5c47f62c29b071c9e34b3dac9f8f
imgeek007 <natlusrun94@gmail.com>
2020-02-22 14:53:54 +0300

    Empty project, next step is add signing viewpager

---
+++ app/src/main/java/com/geektech/mygame/MainActivity.java
@@ -0,0 +1,25 @@
+package com.geektech.mygame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    public void onChooseGameMode(View view) {
+
+    }
+}
